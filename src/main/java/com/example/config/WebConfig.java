cce8c5e9c89c6b8e4eb98f7ccd889e90244a843c
AlexMikhaylov <alx.mikhaylov@gmail.com>
2021-08-14 10:22:12 +0200

    Updated the spring project

---
+++ src/main/java/com/example/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+// To enable Spring MVC support through a Java configuration class, all we have to do is add the @EnableWebMvc annotation
+// This will set up the basic support we need for an MVC project,
+// such as registering controllers and mappings, type converters, validation support, message converters and exception handling.
+@EnableWebMvc
+@Configuration
+// to define and scan additional controller classes
+@ComponentScan(basePackages = { "com.example.controllers" })
+// If we want to customize this configuration, we need to implement the WebMvcConfigurer interface
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("view/index");
+    }
+
+    //In this example, we've registered a ViewResolver bean that will return .jsp views from the /WEB-INF/view directory.
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
+
+        return bean;
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Events</title>
+</head>
+<body>
+<h1>Events list</h1>
+<ul>
+    <li><a href="/events">Click here to get the event list as a model view </a></li>
+    <li><a href="/eventsJson">Click here to get the event list as JSON response</a></li>
+</ul>
+
+</body>
+</html>
